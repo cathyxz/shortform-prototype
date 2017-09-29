c2d033a07e11e5f6b574afdd84f981211a1079bb
Cathy Zhu <cathyxz@gmail.com>
2017-09-29 11:21:08 +0800

    Add basic routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+
 import './App.css';
 import { DependencyStatus } from './components/DependencyStatus.js';
+import { Home } from './components/Home.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <DependencyStatus />
-      </div>
+      <Router>
+        <div className="App">
+          <Route exact path="/" component={Home}/>
+          <Route path="/dependency-status" component={DependencyStatus}/>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class home extends Component {
+
+  render = () => {
+    return (
+      <div className="container">
+          <h1>Home Page</h1>
+          <div>
+              This is the home page there is currently nothing here.
+          </div>
+      </div>
+    );
+  }
+
+}
+
+export const Home = home;
