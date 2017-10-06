8bb3e96e2799dd1ff39d34fbf538e4d8b987a129
Cathy Zhu <cathyxz@gmail.com>
2017-10-06 21:54:45 +0800

    Update homepage with history push

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 
 class home extends Component {
 
+  handleContinueClick = () => {
+    // TODO: dispatch name to redux
+    // TODO: redirect to actual next page
+    this.props.history.push("/personal-info")
+  }
+
   render = () => {
     return (
       <div className="container">
@@ -9,16 +15,15 @@ class home extends Component {
             <div className="centered-text">
               <div className="pt-ui-text-large">
                 Hi there! Who's going to fill out this FAFSA?
-                Please enter your full name.
+                Please enter your first name.
               </div>
               <div className="centered-input">
                 <input class="pt-input" type="text" placeholder="First Name" dir="auto" />
-                <input class="pt-input" type="text" placeholder="Last Name" dir="auto" />
               </div>
             </div>
         </div>
         <div className="bottom-control">
-          <button type="button" class="pt-button pt-intent-primary">
+          <button type="button" class="pt-button pt-intent-primary" onClick={this.handleContinueClick}>
             Continue
             <span class="pt-icon-standard pt-icon-arrow-right pt-align-right"></span>
           </button>
@@ -26,7 +31,6 @@ class home extends Component {
       </div>
     );
   }
-
 }
 
 export const Home = home;
